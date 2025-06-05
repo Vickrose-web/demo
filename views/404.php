8c1a1f8dab19e3703184d5234b094dc057db4827
Vickrose-web <victorsimon200@gmail.com>
2025-06-05 19:02:25 +0100

    First Commit

---
+++ views/404.php
@@ -0,0 +1,12 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Sorry. Not Found.</h1>
+        <p>
+            <a href="/" class="text-blue-500 hover:underline">Go Back to Home Page</a>
+        </p>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
